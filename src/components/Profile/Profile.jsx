dee385b6d35f476199495099f56b41a15a00c636
Mart <marta.nikiel@yahoo.com>
2023-11-07 13:51:53 +0100

    ready task 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,27 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export const Profile = ({ avatar, username, tag, llcation, stats }) => (
-  <div class="profile">
-    <div class="description">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-        class="avatar"
-      />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+  <div className={css.profile}>
+    <div className={css.description}>
+      <img src={avatar} alt="User avatar" className={css.avatar} />
+      <p className={css.profileName}>{username}</p>
+      <p className={css.profileTag}>@{tag}</p>
+      <p className={css.profileLocation}>{location}</p>
     </div>
 
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+    <ul className={css.stats}>
+      <li className={css.followerList}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
       </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
+      <li className={css.viewsList}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
       </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+      <li className={css.likesList}>
+        <span className={stats.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
