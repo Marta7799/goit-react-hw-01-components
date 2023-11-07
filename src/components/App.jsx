42f0790917c1584da6981ee9ebd9ce26708ba1b8
Mart <marta.nikiel@yahoo.com>
2023-11-07 14:59:19 +0100

    changed letter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/statistics.json';
+
 export const App = () => {
   return (
     <>
@@ -11,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </>
   );
 };
