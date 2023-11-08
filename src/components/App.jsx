2ee3de7a30667a84a1ec524eef1be093dc3b57ea
Mart <marta.nikiel@yahoo.com>
2023-11-08 09:11:11 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} /> */}
     </>
   );
 };
