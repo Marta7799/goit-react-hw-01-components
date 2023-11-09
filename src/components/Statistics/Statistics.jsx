a67588f86ad990dd01112a606dbeb1071e92c40b
Mart <marta.nikiel@yahoo.com>
2023-11-09 09:16:13 +0100

    task 2 -ready

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,36 +6,29 @@ function getRandomHexColor() {
   return bgColor;
 }
 
-export const Statistics = ({ title, stats }) => (
-  <div className={css.statisticsContainer}>
+export const Statistics = ({ title = null, stats }) => {
+  const statRender = stats.map(stat => {
+    return (
+      <li
+        style={{ backgroundColor: getRandomHexColor() }}
+        key={stat.id}
+        className={css.item}
+      >
+        {' '}
+        <span className={css.label}>{stat.label}</span>
+        <span className={css.percentage}>{stat.percentage}%</span>
+      </li>
+    );
+  });
+  return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-
-      <ul className={css.statList}>
-        {stats.map(stat => (
-          <li
-            className={title ? css.statListItem : css.statListItemNoTitle}
-            style={{
-              backgroundColor: getRandomHexColor(),
-            }}
-            key={stat.id}
-          >
-            <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}%</span>
-          </li>
-        ))}
-      </ul>
+      <ul className={css.statList}>{statRender}</ul>
     </section>
-  </div>
-);
+  );
+};
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ).isRequired,
+  stats: PropTypes.array.isRequired,
 };
