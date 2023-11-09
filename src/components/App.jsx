884eefc78897b22bc86d1c52c92a9644b8a9ac85
Mart <marta.nikiel@yahoo.com>
2023-11-09 12:39:45 +0100

    ready task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,6 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
       <FriendList friends={friends} />
     </>
   );
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,2 +1,27 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  const friendsRender = friends.map(friend => {
+    return (
+      <li key={friend.id} className={css.item}>
+        <span
+          className={friend.isOnline ? css.statusActive : css.statusInactive}
+        ></span>
+
+        <img
+          className={css.avatar}
+          src={friend.avatar}
+          alt="User avatar"
+          width="48"
+        />
+        <p className={css.name}>{friend.name}</p>
+      </li>
+    );
+  });
+  return <ul className={css.friendList}>{friendsRender}</ul>;
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
