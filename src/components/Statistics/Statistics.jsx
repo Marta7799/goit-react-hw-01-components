b6fda82dac24392888d27607ed6a03949356fb8d
Mart <marta.nikiel@yahoo.com>
2023-11-10 13:30:49 +0100

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -22,7 +22,7 @@ export const Statistics = ({ title = null, stats }) => {
   });
   return (
     <section className={css.statistics}>
-      {title && <h2 className={css.title}>{title}</h2>}
+      {<h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>{statRender}</ul>
     </section>
   );
